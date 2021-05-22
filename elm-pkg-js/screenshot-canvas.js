c347c81727f5d42a35538e26ce9cbc03ff6e32b9
Martin Stewart <MartinSStewart@gmail.com>
2021-05-22 12:25:44 +0200

    Able to display selected image

--- elm-pkg-js/screenshot-canvas.js
+++ elm-pkg-js/screenshot-canvas.js
@@ -10,11 +10,14 @@ function setImage(data) {
     console.log(data);
     var canvas = document.getElementById(data.canvasId);
     var ctx = canvas.getContext('2d');
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     var img = new Image();
 
     img.onload = function(){
       ctx.drawImage(img, 0, 0);
     }
-    img.setAttribute("src", data.image);
-    //img.src = URL.createObjectURL(data.image);
+    //img.setAttribute("src", data.image);
+    var blob = new Blob([data.image.buffer]);
+    img.src = window.URL.createObjectURL(blob);
 }
