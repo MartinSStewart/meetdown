7bf069351760773771d33fd7602ea1aab8839b05
Martin Stewart <MartinSStewart@gmail.com>
2021-06-23 21:43:48 +0200

    Just deploying some changes in order to get the config to update

--- src/NetworkCache.elm
+++ src/NetworkCache.elm
@@ -1,15 +1,10 @@
-module NetworkCache exposing (..)
+module NetworkCache exposing (Cache(..), Request(..), getValue)
 
 import AssocList as Dict exposing (Dict)
 import FrontendUser exposing (FrontendUser)
 import Group exposing (Group)
 import Id exposing (GroupId, Id, UserId)
-
-
-type alias Data =
-    { users : Dict (Id UserId) (Cache FrontendUser)
-    , groups : Dict GroupId (Cache Group)
-    }
+import Types exposing (LoadedFrontend)
 
 
 type Cache a
@@ -23,8 +18,42 @@ type Request id
     | Request id
 
 
-getValue : id -> Dict id (Cache value) -> ( Maybe value, Dict id (Cache value), Request id )
-getValue id dict =
+type Object valueBuilder requestBuilder state
+    = Object (Maybe valueBuilder) requestBuilder state
+
+
+start valueBuilder requestBuilder state =
+    Object (Just valueBuilder) requestBuilder state
+
+
+getValue :
+    id
+    -> (state -> Dict id (Cache a))
+    -> (Dict id (Cache a) -> state -> state)
+    -> Object (a -> value) (Request id -> request) state
+    -> Object value request state
+getValue id getter setter (Object maybeValueBuilder requestBuilder state) =
+    let
+        ( maybeValue, newDict, request ) =
+            getValueHelper id (getter state)
+    in
+    Object
+        (Maybe.map2
+            (\valueBuilder value -> valueBuilder value)
+            maybeValueBuilder
+            maybeValue
+        )
+        (requestBuilder request)
+        (setter newDict state)
+
+
+a model =
+    start Tuple.pair Tuple.pair
+        |> getValue (Id.groupIdFromInt 0) .cachedGroups (\dict a -> { a | cachedGroups = dict })
+
+
+getValueHelper : id -> Dict id (Cache value) -> ( Maybe value, Dict id (Cache value), Request id )
+getValueHelper id dict =
     case Dict.get id dict of
         Just (Cached _ value) ->
             ( Just value, dict, NoRequest )
